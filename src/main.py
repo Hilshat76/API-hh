174ea5d3d31ab046d340a296e2b1ea4f64b1424b
Alexander Gedz <gedz.buh@yandex.ru>
2024-03-31 17:45:55 +0300

    added exception handling for top_n added keyword_vacancy

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.class_vacancies_hh import VacanciesHH
 
 def get_value(dictionary, *keys):
     """
-    Возвращает значение из словаря по заданному пути ключей.
+    Возвращает значение из словаря по заданному пути ключей
     :param dictionary: словарь, из которого мы получаем значение
     :param keys: переменное количество ключей в виде строки, определяющих путь к значению.
     :return: Значение из словаря, связанное с заданным путем ключей.
@@ -19,47 +19,75 @@ def get_value(dictionary, *keys):
 
 
 def user_interaction():
-    """
-    Функция для работы с запросами пользователя
-    """
     print('Привет! С помощью этого приложения поиск вакансий на Hh.ru станет намного проще.')
+    name_vacancy = input('Введите название вакансии: ')
+    keyword_vacancy = input('Введите ключевые слова для фильтрации вакансий (разделяйте пробелами): ').split()
+
     try:
-        name_vacancy = input('Введите название вакансии: ')
         top_n = int(input('Введите количество вакансий для отображения по убыванию зарплаты: '))
+        if top_n <= 0:
+            raise ValueError
+    except ValueError:
+        print('Получено некорректное значение. Будут выданы все результаты:')
+        top_n = None
 
-        vacancy_hh = HeadHunterRuAPI()
-        all_vacancy = vacancy_hh.getting_vacancies(name_vacancy)
+    vacancy_hh = HeadHunterRuAPI()
+    all_vacancy = vacancy_hh.getting_vacancies(name_vacancy)
 
-        # Фильтрация вакансий по валюте RUR
-        all_vacancy = [vacancy for vacancy in all_vacancy.get('items') if get_value(vacancy, 'salary', 'currency') == 'RUR']
+    # Фильтрация вакансий по валюте RUR (т.к ищем в России)
+    all_vacancy = [vacancy for vacancy in all_vacancy.get('items') if get_value(vacancy, 'salary', 'currency') == 'RUR']
+
+    if len(all_vacancy) == 0:
+        print("По вашему запросу вакансий не найдено")
+    else:
+        print(f"Всего количество вакансий по запросу '{name_vacancy}': {len(all_vacancy)}")
+        print(f"Топ {top_n or len(all_vacancy)} вакансий по зарплате:")
 
         good_vacancy = []
 
-        print(f"Всего количество вакансий по запросу '{name_vacancy}': {len(all_vacancy)}")
-        print(f"Топ {min(top_n, len(all_vacancy))} вакансий по зарплате:")
+        if not keyword_vacancy:
+            top_n_vacancy = top_n or len(all_vacancy)
+            for vacancy in all_vacancy[:top_n_vacancy]:
+                try:
+                    name = get_value(vacancy, 'name')
+                    area = get_value(vacancy, 'area', 'name')
+                    salary_from = get_value(vacancy, 'salary', 'from')
+                    salary_to = get_value(vacancy, 'salary', 'to')
+                    salary_currency = get_value(vacancy, 'salary', 'currency')
+                    requirement = get_value(vacancy, 'snippet', 'requirement')
+                    alternate_url = get_value(vacancy, 'alternate_url')
+                    good_vacancy.append(
+                        VacanciesHH(name, area, salary_from, salary_to, salary_currency, requirement, alternate_url))
+                except Exception as e:
+                    print(f"Ошибка обработки вакансии: {e}")
+        else:
+            for vacancy in all_vacancy:
+                try:
+                    name = get_value(vacancy, 'name')
+                    area = get_value(vacancy, 'area', 'name')
+                    salary_from = get_value(vacancy, 'salary', 'from')
+                    salary_to = get_value(vacancy, 'salary', 'to')
+                    salary_currency = get_value(vacancy, 'salary', 'currency')
+                    requirement = get_value(vacancy, 'snippet', 'requirement')
+                    alternate_url = get_value(vacancy, 'alternate_url')
+                    if any(keyword.lower() in str(vacancy).lower() for keyword in keyword_vacancy):
+                        good_vacancy.append(
+                            VacanciesHH(name, area, salary_from, salary_to, salary_currency, requirement,
+                                        alternate_url))
+                except Exception as e:
+                    print(f"Ошибка обработки вакансии: {e}")
 
-        # использование {} для создания пустого словаря вместе с методом get() обеспечивает
-        # безопасный доступ к значениям словаря и предотвращает возможные ошибки
-        # при отсутствии ключей или пустых значений в словаре.
-        for vacancy in all_vacancy:
-            name = get_value(vacancy, 'name')
-            area = get_value(vacancy, 'area', 'name')
-            salary_from = get_value(vacancy, 'salary', 'from')
-            salary_to = get_value(vacancy, 'salary', 'to')
-            salary_currency = get_value(vacancy, 'salary', 'currency')
-            requirement = get_value(vacancy, 'snippet', 'requirement')
-            alternate_url = get_value(vacancy, 'alternate_url')
+        if len(good_vacancy) == 0:
+            top_n_vacancy = top_n or len(all_vacancy)
+            print(f"Ключевые слова не найдены. Будут выданы все результаты: {top_n_vacancy} вакансий.")
+            good_vacancy = all_vacancy[:top_n_vacancy]
+        else:
+            top_vacancy = sorted(good_vacancy, key=lambda x: x.salary_to if x.salary_to is not None else 0,
+                                 reverse=True)
+            good_vacancy = top_vacancy[:top_n]
 
-            good_vacancy.append(
-                VacanciesHH(name, area, salary_from, salary_to, salary_currency, requirement, alternate_url))
-
-        top_vacancy = sorted(good_vacancy, key=lambda x: x.salary_to if x.salary_to is not None else 0, reverse=True)
-
-        for vacancy in top_vacancy[:min(top_n, len(top_vacancy))]:
+        for vacancy in good_vacancy:
             print(vacancy)
-
-    except TypeError as e:
-        print(f"{e}")
 
 
 if __name__ == "__main__":
